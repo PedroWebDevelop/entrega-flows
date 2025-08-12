572a93a523c35300245283a946382110efac71f8
pedroWebDevelop <pedromarquesn@agilsystem.com>
2025-08-12 06:30:12 -0300

    Feature: termianndo de estudar sobre fluig e arquiterura mais profissional encontrada,

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
+const App = {
+		init() {
+				this.loadLayout('src/layout/LayoutHome.html');
+		},
 
-import component1 from './libs/components/inputs/Input.js';
-import component2 from './libs/components/forms/BasicForm.js';
+		loadLayout(path) {
+				fetch(path)
+								.then(response => response.text())
+								.then(html => {
 
-document.querySelector('.fluig-style-guide').innerHTML = `
-  ${component1()}
-  ${component2()}
-`;
+										const temp = document.createElement('div');
+										temp.innerHTML = html.trim();
 
+
+										const fluigWrapper = temp.querySelector('.fluig-style-guide');
+
+										document.getElementById('app').innerHTML = fluigWrapper ? fluigWrapper.innerHTML : temp.innerHTML;
+								})
+								.catch(err => {
+										console.error('Erro ao carregar layout:', err);
+										document.getElementById('app').innerHTML = `<p>Erro ao carregar layout.</p>`;
+								});
+		}
+};
+
+document.addEventListener('DOMContentLoaded', () => App.init());
